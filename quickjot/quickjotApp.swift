cd5253df6806e83e11ea38a36eeadc9f44ec6ef0
Fengji Zhang <thewyze9@outlook.com>
2024-02-24 11:04:24 -0500

    Initial Commit

---
+++ quickjot/quickjotApp.swift
@@ -0,0 +1,17 @@
+//
+//  quickjotApp.swift
+//  quickjot
+//
+//  Created by Fengji Zhang on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct quickjotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
