d830da6efbabcb226477b03502d7b850c493230e
Fengji Zhang <thewyze9@outlook.com>
2024-02-24 11:42:23 -0500

    adding views

--- quickjot/ContentView.swift
+++ quickjot/ContentView.swift
@@ -9,14 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TabView {
+                HomeView()
+                    .tabItem {
+                        Label("Home", systemImage: "person")
+                    }
+
+                ListView()
+                    .tabItem {
+                        Label("List", systemImage: "book")
+                    }
+            }
+            
         }
-        .padding()
-    }
 }
 
 #Preview {
---
+++ quickjot/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  quickjot
+//
+//  Created by Fengji Zhang on 2/24/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
